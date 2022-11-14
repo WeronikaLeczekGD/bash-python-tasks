ce8e5f9edf405e95dedb4cab2b0d914f9e8bd177
WeronikaLeczekGD <wleczek@griddynamics.com>
2022-11-14 14:24:51 +0100

    ignoring '-'

--- p_task3.py
+++ p_task3.py
@@ -1,27 +1,30 @@
-
 import sys
 
 with open(sys.argv[1], 'r') as f:
-
+    
     user_agent_dict = {}
-
+    
     for line in f:
-
+        
         line_list = line.split()
         try:
             user_agent = line_list[11]
         except:
             pass
-
+        
+        if "-" in user_agent:
+            continue
+        
         if user_agent in user_agent_dict:
             user_agent_dict[user_agent] += 1
-
+        
         else:
             user_agent_dict[user_agent] = 1
-
+            
     print("Total number of different user agents: {}".format(len(user_agent_dict)))
-
+    
     for key, value in user_agent_dict.items():
         print("{}: {}".format(key, value))
         
+            
 
